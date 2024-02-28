9979a90aebfd7e832495c69796c4a75677d12a7c
Artur Malinowski <arturrmalinowski@gmail.com>
2024-02-29 00:44:59 +0100

    Layout Created with bootstrap

--- src/app/crud/crud.module.ts
+++ src/app/crud/crud.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CRUDRoutingModule } from './crud-routing.module';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductFormComponent } from './product-form/product-form.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
-    CRUDRoutingModule
+    CRUDRoutingModule,
+    AgGridModule
   ]
 })
 export class CRUDModule { }
--- src/app/crud/product-list/product-list.component.ts
+++ src/app/crud/product-list/product-list.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { AgGridAngular } from 'ag-grid-angular'; // AG Grid Component
+import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent {
+  rowData = [
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+  ];
 
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ];
 }
