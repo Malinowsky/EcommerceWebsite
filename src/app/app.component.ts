ecbedac7469d65bad5c27003dddbd80fdd75a196
Artur Malinowski <arturrmalinowski@gmail.com>
2024-02-29 14:50:28 +0100

    Customize a table/ add buttons/ fetch data from php

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from "@angular/core";
-import {RouterModule, RouterOutlet } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  [x: string]: any;
   title = 'EcommerceWebsite';
 }
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
--- src/app/crud/product-list/product-list.component.ts
+++ src/app/crud/product-list/product-list.component.ts
@@ -10,21 +10,63 @@ import { CRUDService } from '../services/crud.service';
   styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent {
-  rowData = [
-    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
-    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
-    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
-  ];
-
   // Column Definitions: Defines the columns to be displayed.
   colDefs: ColDef[] = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' },
-    { field: 'electric' },
+    {
+      field: 'p_name',
+      headerName: 'Name',
+      sortable: true,
+      headerClass: 'header-cell',
+    },
+    {
+      field: 'p_description',
+      headerName: 'Desc',
+      sortable: true,
+      headerClass: 'header-cell',
+    },
+    {
+      field: 'p_price',
+      headerName: 'Price',
+      sortable: true,
+      headerClass: 'header-cell',
+    },
+    {
+      field: '',
+      headerName: 'Actions',
+      headerClass: 'header-cell',
+      width: 250,
+      cellRenderer: this.actionRender,
+    },
   ];
+  rowData: any = [];
+  gridOptions = {
+    rowHeight: 50,
+  };
 
+  productList: any = [];
+  productListSubscribe: any;
   constructor(private crudService: CRUDService) {}
 
-  ngOnit(): void {}
+  ngOnInit(): void {
+    this.getProductList();
+  }
+  getProductList() {
+    this.productListSubscribe = this.crudService
+      .loadProducts()
+      .subscribe((data) => {
+        this.productList = data;
+        this.rowData = data;
+        console.log('data', data);
+      });
+  }
+
+  actionRender(params: any) {
+    let div = document.createElement('div');
+    let htmlCode =
+      '<button type ="button" class="btn btn-success">View</button>\n' +
+      '<button type ="button" class="btn btn-danger">Delete</button>\n' +
+      '<button type ="button" class="btn btn-warning">Edit</button>';
+    div.innerHTML = htmlCode;
+    return div;
+  }
 }
